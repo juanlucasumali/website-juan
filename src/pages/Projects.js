20071ef802c3a1156fd6d10cc3acc8eb57a6d45f
Juan Lucas Umali <juanlucasumali@berkeley.edu>
2022-10-03 23:50:52 -0700

    Integrated Airtable API!

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,34 +1,45 @@
 import React from "react";
 import ProjectItem from "/Users/juanlucasumali/Desktop/berkeley/codebase/mentoredfa22/website-juan/src/components/projectItem.js";
 import "/Users/juanlucasumali/Desktop/berkeley/codebase/mentoredfa22/website-juan/src/pages/Projects.css"
+import axios from "axios";
 
 export default function Projects(){
+    
+    
+    const [proj, setProj] = React.useState(null);
+    async function fetchData() {
+    const res = await axios.get(
+      "https://api.airtable.com/v0/appr21utG5vIoAI3c/Projects?maxRecords=10&view=Grid%20view",
+      {
+        headers: {
+          authorization: `Bearer keyJff48m18Xx0Ndg`,
+        },
+      }
+    );
+    console.log(res.data);
+    setProj(res.data.records);
+  }
+
+  React.useEffect(() => {
+    fetchData();
+  }, []);
+
+  if (!proj) {
+    return <div>Loading...</div>;
+  }
+    
+    
+    
     return(
         <div>
                 <div id="projects" className="text">
                     <div id="title" >projects</div>
                     <div className="resume">
-                            <ProjectItem
-                                date="February 2022 — March 2022"
-                                project_title="KNN and Decision Tree ML Models"
-                                description_1="- Created and implemented KNN and decision tree from scratch using Python"
-                                description_2=""
-                                description_3=""
-                            />
-                            <ProjectItem
-                                date="April 2022 —  May 2022"
-                                project_title="Neuroevolution Simulation"
-                                description_1="- Simulated neuroevolution in Unity 3D using C# and the Unity ML-Agents toolkit"
-                                description_2="- Applied a genetic algorithm to a population of artificial neural networks assigned to simulated cars to emulate an ‘evolving’ population of self-driving car AIs"
-                                description_3=""
-                            />
-                            <ProjectItem
-                                date="August 2022 —  September 2022"
-                                project_title="Chord Progression Generator"
-                                description_1="- Created and deployed a website in HTML, CSS, and JavaScript which generates a chord progression"
-                                description_2=""
-                                description_3=""
-                            />
+
+                        {proj.map((record) => (
+                        <ProjectItem key={record.id} date={record.fields.date} project_title={record.fields.project_title} description_1={record.fields.description_1} description_2={record.fields.description_2} description_3={record.fields.description_3}/>
+                        ))}
+
                     </div>
                 </div>
             
